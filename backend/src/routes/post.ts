34266148f7709b30693e693a50735d7e8420eeb0
Meru45 <87946072+Meru45@users.noreply.github.com>
2024-05-24 16:45:00 +0530

    Code Implementation: update, get, getAll posts routes created in post.ts

--- backend/src/routes/post.ts
+++ backend/src/routes/post.ts
@@ -27,12 +27,71 @@ postRouter.post("/", async (c) => {
   }
 });
 
-postRouter.put("/", (c) => {
-  return c.json({ msg: "this is the blog route" });
+postRouter.put("/:id", async (c) => {
+  const id = c.req.param("id");
+  if (!id) {
+    c.status(400);
+    return c.json({ msg: "Please put the id of the post in the param" });
+  }
+  const body = await c.req.json();
+  if (!body.title || !body.content) {
+    c.status(400);
+    return c.json({ msg: "Title/Content cannot be empty" });
+  }
+  const Post = c.get("post");
+  const userId = c.get("userId");
+
+  try {
+    await Post.update({
+      where: {
+        id: id,
+        authorId: userId,
+      },
+      data: {
+        title: body.title,
+        content: body.content,
+      },
+    });
+
+    c.status(200);
+    return c.json({ msg: "Post updated" });
+  } catch (error) {
+    c.status(500);
+    return c.json({ msg: "Error occured while updating the post in database" });
+  }
 });
 
-postRouter.get("/:id", (c) => {
-  return c.json({ msg: "this is the blog route" });
+postRouter.get("/:id", async (c) => {
+  const id = c.req.param("id");
+  const Post = c.get("post");
+  try {
+    const post = await Post.findFirst({
+      where: {
+        id: id,
+      },
+    });
+    c.status(400);
+    return c.json({ post });
+  } catch (error) {
+    c.status(500);
+    return c.json({
+      msg: `Error occured while fetching the post with id ${id}`,
+    });
+  }
+});
+
+//TODO: Add pagination
+postRouter.get("/", async (c) => {
+  const Post = c.get("post");
+  console.log(Post);
+  try {
+    const posts = await Post.findMany({});
+    c.status(400);
+    return c.json({ posts });
+  } catch (error) {
+    c.status(500);
+    return c.json({ msg: "Error occured while fetching posts" });
+  }
 });
 
 export default postRouter;
